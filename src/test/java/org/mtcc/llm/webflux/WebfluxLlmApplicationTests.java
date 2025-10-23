68a61d8ae5eb8d3c0d353be8c0e2cf8040612340
jaeyounglee <ljyda214@gmail.com>
2025-10-23 23:45:26 +0900

    init

---
+++ src/test/java/org/mtcc/llm/webflux/WebfluxLlmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mtcc.llm.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxLlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

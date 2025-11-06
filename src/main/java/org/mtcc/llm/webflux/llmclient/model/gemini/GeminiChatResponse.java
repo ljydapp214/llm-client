42d7eb9965ec1ee0afd65d3493a93d9edcf380cb
jaeyounglee <ljyda214@gmail.com>
2025-11-06 23:55:14 +0900

    feat: SingleText 가 없는 케이스에 대한 예외처리 진행

--- src/main/java/org/mtcc/llm/webflux/llmclient/model/gemini/GeminiChatResponse.java
+++ src/main/java/org/mtcc/llm/webflux/llmclient/model/gemini/GeminiChatResponse.java
@@ -3,11 +3,15 @@ package org.mtcc.llm.webflux.llmclient.model.gemini;
 import java.io.Serializable;
 import java.util.List;
 
+import org.mtcc.llm.webflux.exception.BaseException;
+import org.mtcc.llm.webflux.exception.ErrorCode;
+
 public record GeminiChatResponse(List<GeminiCandidate> candidates) implements Serializable {
 	public String getSingleText() {
 		return candidates.stream().findFirst()
 			.flatMap(candidate -> candidate.content().parts().stream().findFirst()
 				.map(GeminiPart::text))
-			.orElseThrow();
+			.orElseThrow(
+				() -> new BaseException("[GptResponse] There is no candidate", ErrorCode.GEMINI_RESPONSE_ERROR));
 	}
 }
--- src/main/java/org/mtcc/llm/webflux/llmclient/model/gpt/GptChatResponse.java
+++ src/main/java/org/mtcc/llm/webflux/llmclient/model/gpt/GptChatResponse.java
@@ -3,10 +3,18 @@ package org.mtcc.llm.webflux.llmclient.model.gpt;
 import java.io.Serializable;
 import java.util.List;
 
+import org.mtcc.llm.webflux.exception.BaseException;
+import org.mtcc.llm.webflux.exception.ErrorCode;
+
 public record GptChatResponse(
 	List<GptChoice> choices
 ) implements Serializable {
 	public GptChoice getSingleChoice() {
-		return choices.stream().findFirst().orElseThrow();
+		return choices.stream().findFirst().orElseThrow(() ->
+			new BaseException("[GptResponse] There is no choices", ErrorCode.GPT_RESPONSE_ERROR));
+	}
+
+	public boolean isNotFinish() {
+		return getSingleChoice().finish_reason() == null;
 	}
 }

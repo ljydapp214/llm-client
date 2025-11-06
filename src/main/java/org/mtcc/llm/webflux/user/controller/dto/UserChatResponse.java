5e32f1c184604cb996fdc43bef02e8c966beeeea
jaeyounglee <ljyda214@gmail.com>
2025-11-06 23:56:32 +0900

    chg: 신규 필드 추가

--- src/main/java/org/mtcc/llm/webflux/user/controller/dto/UserChatResponse.java
+++ src/main/java/org/mtcc/llm/webflux/user/controller/dto/UserChatResponse.java
@@ -3,10 +3,16 @@ package org.mtcc.llm.webflux.user.controller.dto;
 import java.io.Serial;
 import java.io.Serializable;
 
+import org.mtcc.llm.webflux.exception.CommonError;
+
 public record UserChatResponse(
-	String response
+	String response,
+	CommonError error
 ) implements Serializable {
 	@Serial
 	private static final long serialVersionUID = -2479640483541743119L;
 
+	public UserChatResponse(String response) {
+		this(response, null);
+	}
 }
--- src/main/java/org/mtcc/llm/webflux/user/service/dto/LlmChatResponse.java
+++ src/main/java/org/mtcc/llm/webflux/user/service/dto/LlmChatResponse.java
@@ -2,13 +2,28 @@ package org.mtcc.llm.webflux.user.service.dto;
 
 import java.io.Serializable;
 
+import org.mtcc.llm.webflux.exception.CommonError;
 import org.mtcc.llm.webflux.llmclient.model.gemini.GeminiChatResponse;
 import org.mtcc.llm.webflux.llmclient.model.gpt.GptChatResponse;
 
-public record LlmChatResponse(String llmResponse) implements Serializable {
+public record LlmChatResponse(
+	String llmResponse,
+	CommonError error
+) implements Serializable {
+	public LlmChatResponse(CommonError error) {
+		this("", error);
+	}
+
+	public LlmChatResponse(String llmResponse) {
+		this(llmResponse, null);
+	}
 
 	public static LlmChatResponse from(GptChatResponse gptChatResponse) {
 		return new LlmChatResponse(gptChatResponse.getSingleChoice().message().content());
+	}
+
+	public static LlmChatResponse fromDelta(GptChatResponse gptChatResponse) {
+		return new LlmChatResponse(gptChatResponse.getSingleChoice().delta().content());
 	}
 
 	public static LlmChatResponse from(GeminiChatResponse geminiChatResponse) {
